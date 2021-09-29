a2960fb84054cfdade99a2296b3b2efff42eaf64
Jan Willem Henckel <jan@mpfilm.de>
2021-09-29 13:52:09 +0200

    Update render.js

--- src/render.js
+++ src/render.js
@@ -2,4 +2,6 @@ const root = document.querySelector('#root');
 
 export const render = (htmlString) => {
   root.innerHTML = htmlString;
+  
+  // TODO: If your markup needs event listeners, add them here. (You can import the handler functions from another module!)
 };
